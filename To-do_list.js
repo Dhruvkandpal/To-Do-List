a056410ad2500757fed43f2923f8dd419549c100
Dhruvkandpal <43724393+Dhruvkandpal@users.noreply.github.com>
2018-09-30 23:30:11 +0530

    Add files via upload

---
+++ To-do_list.js
@@ -0,0 +1,44 @@
+var items = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < items.length; i++) {
+  var span = document.createElement('span');
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  items[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Add some task!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
